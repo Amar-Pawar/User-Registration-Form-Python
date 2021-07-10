3029f98598c7c20038bd7b73d5a5973cacef3891
Amar-Pawar <pawaramar.pawar@gmail.com>
2021-07-10 15:09:17 +0530

    [Amar Pawar] Refactor:Removed hard-coded values from testing file

--- Test/pytest_registration.py
+++ Test/pytest_registration.py
@@ -3,7 +3,7 @@
 @Author: Amar Pawar
 @Date: 2021-07-07
 @Last Modified by: Amar Pawar
-@Last Modified time: 2021-07-09
+@Last Modified time: 2021-07-10
 @Title : Test cases for user registration system using pytest
 /**********************************************************************************
 '''
@@ -25,115 +25,6 @@ def read_json():
     data = json.load(f)
     return data
 
-
-# test case for first name
-def test_first_name_should_return_true():
-    """
-    Description:
-        Given valid name should return true and pass the test. 
-    """
-    assert validate_name("Amar")
-    assert validate_name("Isc")
-# negative test case for first name
-def test_first_name_should_return_false():
-    """
-    Description:
-        Given invalid name should return false and pass the test. 
-    """
-    with pytest.raises(Exception) as exc_info:
-        assert validate_name("amar")
-        assert validate_name("!mar")
-        assert validate_name("1amar")
-        assert validate_name("AMmar")
-        assert validate_name("Amar@")
-        assert validate_name("Amar1")
-# test case for last name
-def test_last_name_should_return_true():
-    """
-    Description:
-        Given valid last name should return true and pass the test. 
-    """
-    assert validate_name("Pawar")
-    assert validate_name("Isc")
-# negative test case for last name
-def test_last_name_should_return_flase():
-    """
-    Description:
-        Given invalid last name should return false and pass the test. 
-    """
-    with pytest.raises(Exception) as exc_info:
-        assert validate_name("pawar")
-        assert validate_name("p!mar")
-        assert validate_name("1mhatre")
-        assert validate_name("PAtil")
-        assert validate_name("Mhatre@")
-        assert validate_name("paWar")
-# test case for email
-def test_email_should_return_true():
-    """
-    Description:
-        Given valid email should return true and pass the test. 
-    """
-    assert email_validate("amar.ar92@ymail.com")
-    assert email_validate("amar.ar92@gmail.in")
-    assert email_validate("amar@co.uk")
-    assert email_validate("pawaramar.pawar@gmail.com")
-    assert email_validate("amar@co.uk")
-    assert email_validate("hr@outlook.in")
-# negative test case for email
-def test_email_should_return_false():
-    """
-    Description:
-        Given invalid email should return false and pass the test. 
-    """
-    with pytest.raises(Exception) as exc_info:
-        assert email_validate("@mar.ar92@@ymail.com")
-        assert email_validate("1mar.ar92@ymail.com")
-        assert email_validate("amar.ar92@@ymail.com")
-        assert email_validate("am#ar.ar92@ymail.com")
-# test case for mobile number
-def test_number_should_return_true():
-    """
-    Description:
-        Given valid mobile number should return true and pass the test. 
-    """
-    assert number_validation("91 9028626816")
-    assert number_validation("83 8787878780")
-    assert number_validation("72 8989894543")
-    assert number_validation("24 1234345656")
-# negative test case for mobile number
-def test_number_should_return_false():
-    """
-    Description:
-        Given invalid mobile should return false and pass the test. 
-    """
-    with pytest.raises(Exception) as exc_info:
-        assert number_validation("909899098909")
-        assert number_validation("91 0909876789")
-        assert number_validation("01 9090987678")
-# test case for password
-def test_password_should_return_true():
-    """
-    Description:
-        Given valid password should return true and pass the test. 
-    """
-    assert password_validate("amaArxyx@12")
-    assert password_validate("123x@yAzamar")
-    assert password_validate("Amar@pawar123")
-    assert password_validate("amaArxyx@1")
-# negative test case for password
-def test_password_should_return_false():
-    """
-    Description:
-        Given invalid password should return false and pass the test. 
-    """
-    with pytest.raises(Exception) as exc_info:
-        assert password_validate("@amar")
-        assert password_validate("12@adas345h")
-        assert password_validate("@assdfdgb#")
-        assert password_validate("@assdf1dgb#")
-        assert password_validate("@aAssdfdgb")
-
 # test case for valid name by reading json file
 def test_multiple_names_should_return_true():
     """
@@ -154,6 +45,27 @@ def test_multiple_names_should_return_false():
     with pytest.raises(Exception) as exc_info:
         for name in sample_name_list['invalid_name']:
             assert validate_name(name)
+
+# test case for valid last name by reading json file
+def test_multiple_last_names_should_return_true():
+    """
+    Description:
+        Given valid last name samples from json file should return false and pass the test. 
+    """
+    sample_last_name_list = read_json()
+    for last_name in sample_last_name_list['valid_last_name']:
+        assert validate_name(last_name)
+
+# test case for invalid last_name by reading json file
+def test_multiple_last_names_should_return_false():
+    """
+    Description:
+        Given invalid last name samples from json file should return false and pass the test.
+    """
+    sample_last_name_list = read_json()
+    with pytest.raises(Exception) as exc_info:
+        for last_name in sample_last_name_list['invalid_last_name']:
+            assert validate_name(last_name)
 
 # test case for valid email by reading json file
 def test_multiple_emails_should_return_true():
--- Test/unittest_registration.py
+++ Test/unittest_registration.py
@@ -3,7 +3,7 @@
 @Author: Amar Pawar
 @Date: 2021-07-07
 @Last Modified by: Amar Pawar
-@Last Modified time: 2021-07-09
+@Last Modified time: 2021-07-10
 @Title : Test cases for user registration system using unittest
 /**********************************************************************************
 '''
@@ -20,6 +20,8 @@ class TestCalc(unittest.TestCase):
         self.data = json.load(self.f)
         self.valid_name_list = self.data['valid_name']
         self.invalid_name_list = self.data['invalid_name']
+        self.valid_last_name_list = self.data['valid_last_name']
+        self.invalid_last_name_list = self.data['invalid_last_name']
         self.valid_email_list = self.data['valid_email']
         self.invalid_email_list = self.data['invalid_email']
         self.valid_number_list = self.data['valid_number']
@@ -27,106 +29,6 @@ class TestCalc(unittest.TestCase):
         self.valid_password_list = self.data['valid_password']
         self.invalid_password_list = self.data['invalid_password']
         self.f.close()
-    # test case for first name
-    def test_first_name_should_return_true(self):
-        """
-        Description:
-            This method will test if input first is valid or not
-            Given name should return true and pass the test. 
-        """
-        self.assertTrue(validate_name("Isa"))
-        self.assertTrue(validate_name("Amar"))
-    # negative test case for first name
-    def test_first_name_should_return_false(self):
-        """
-        Description:
-            Given invalid name should return false and pass the test. 
-        """
-        self.assertFalse(validate_name("amar"))
-        self.assertFalse(validate_name("sa"))
-        self.assertFalse(validate_name("123sa"))
-        self.assertFalse(validate_name("Ia"))
-        self.assertFalse(validate_name("Amar123"))
-        self.assertFalse(validate_name("Am@1r"))
-        self.assertFalse(validate_name("AMar"))
-    # test case for last name
-    def test_last_name_should_return_true(self):
-        """
-        Description:
-            Given valid name should return true and pass the test. 
-        """
-        self.assertTrue(validate_name("Pawar"))
-        self.assertTrue(validate_name("Zin"))
-    # negative test case for first name
-    def test_last_name_should_return_false(self):
-        """
-        Description:
-            Given invalid name should return false and pass the test. 
-        """
-        self.assertFalse(validate_name("pawar"))
-        self.assertFalse(validate_name("ab"))
-        self.assertFalse(validate_name("123sa"))
-        self.assertFalse(validate_name("Ia"))
-        self.assertFalse(validate_name("Pawar123"))
-    # test case for email
-    def test_email_should_return_true(self):
-        """
-        Description:
-            Given valid email should return true and pass the test. 
-        """
-        self.assertTrue(email_validate("amar.ar92@ymail.com"))
-        self.assertTrue(email_validate("amar.ar92@gmail.in"))
-        self.assertTrue(email_validate("amar@co.uk"))
-        self.assertTrue(email_validate("pawaramar.pawar@gmail.com"))
-        self.assertTrue(email_validate("hr@outlook.in"))    
-    # negative test case for email
-    def test_email_should_return_false(self):
-        """
-        Description:
-            Given invalid email should return false and pass the test. 
-        """
-        self.assertFalse(email_validate("1mar.ar92@ymail.com"))
-        self.assertFalse(email_validate("amar.ar92@@ymail.com"))
-        self.assertFalse(email_validate("am#ar.ar92@ymail.com"))
-    # test case for email
-    def test_number_should_return_true(self):
-        """
-        Description:
-            Given valid number should return true and pass the test. 
-        """
-        self.assertTrue(number_validation("91 9028626816"))
-        self.assertTrue(number_validation("11 7878787667"))
-        self.assertTrue(number_validation("74 7676543907"))
-        self.assertTrue(number_validation("19 6776907565"))
-    # negative test case for email
-    def test_number_should_return_false(self):
-        """
-        Description:
-            Given invalid number should return false and pass the test. 
-        """
-        self.assertFalse(number_validation("91909090987"))
-        self.assertFalse(number_validation("01 7878786567"))
-        self.assertFalse(number_validation("91 0909009876"))
-    # test case for password
-    def test_password_should_return_true(self):
-        """
-        Description:
-            Given valid password should return true and pass the test. 
-        """
-        self.assertTrue(password_validate("amaArxyx@12"))
-        self.assertTrue(password_validate("123@xAyzamar"))
-        self.assertTrue(password_validate("Amar@pawar123"))
-        self.assertTrue(password_validate("Amar123@pawar"))
-    # negative test case for password
-    def test_password_should_return_false(self):
-        """
-        Description:
-            Given invalid password should return false and pass the test. 
-        """
-        self.assertFalse(password_validate("@amarfg"))
-        self.assertFalse(password_validate("amar#gsjjd"))
-        self.assertFalse(password_validate("87789jhsgg"))
-        self.assertFalse(password_validate("Amar@jhsgg"))
 
     # test case for valid first name by reading json file
     def test_first_name_by_reading_json_should_return_true(self):
@@ -145,6 +47,25 @@ class TestCalc(unittest.TestCase):
         """
         for name in self.invalid_name_list:
             self.assertFalse(validate_name(name))
+
+    # test case for valid last name by reading json file
+    def test_last_name_by_reading_json_should_return_true(self):
+        """
+        Description:
+            Given valid last name samples from json file should return false and pass the test. 
+        """
+        for last_name in self.valid_last_name_list:
+            self.assertTrue(validate_name(last_name))
+
+    # test case for invalid last name by reading json file
+    def test_last_name_by_reading_json_should_return_false(self):
+        """
+        Description:
+            Given invalid last name samples from json file should return false and pass the test.
+        """
+        for last_name in self.invalid_last_name_list:
+            self.assertFalse(validate_name(last_name))
+
 
     # test case for valid email by reading json file
     def test_email_by_reading_json_should_return_true(self):
